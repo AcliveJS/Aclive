23aa4e87aafa8f8abbc72cff5f4d34f12203e87f
Aclive <wallace.robert@hotmail.com>
2016-06-04 13:16:23 -0300

    040620161316

--- js/Aclive.js
+++ js/Aclive.js
@@ -881,6 +881,176 @@ function Inicio()
 			gui.open(); 
 }
 
+// Rotinas externas - pesquisa da internet .................................................................................................................................................
+ 
+function Texto2D( message, x, y, z, EL, EA, parameters ) 
+{ 
+     if ( parameters === undefined ) parameters = {}; 
+      
+     var fontface = parameters.hasOwnProperty("fontface") ?  
+         parameters["fontface"] : "Arial"; 
+      
+     var fontsize = parameters.hasOwnProperty("fontsize") ?  
+         parameters["fontsize"] : 18; 
+      
+     var borderThickness = parameters.hasOwnProperty("borderThickness") ?  
+         parameters["borderThickness"] : 4; 
+      
+     var borderColor = parameters.hasOwnProperty("borderColor") ? 
+         parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 }; 
+      
+     var fillColor = parameters.hasOwnProperty("fillColor") ? 
+         parameters["fillColor"] : undefined; 
+  
+     var textColor = parameters.hasOwnProperty("textColor") ? 
+         parameters["textColor"] : { r:0, g:0, b:255, a:1.0 }; 
+  
+     var radius = parameters.hasOwnProperty("radius") ? 
+                 parameters["radius"] : 6; 
+  
+     var vAlign = parameters.hasOwnProperty("vAlign") ? 
+                         parameters["vAlign"] : "center"; 
+  
+     var hAlign = parameters.hasOwnProperty("hAlign") ? 
+                         parameters["hAlign"] : "center"; 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+     var canvas = document.createElement('canvas'); 
+     var context = canvas.getContext('2d'); 
+      
+     // set a large-enough fixed-size canvas  
+     canvas.width = WLargura; 
+     canvas.height = WAltura; 
+      
+     context.font = fontsize + "px " + fontface; 
+     context.textBaseline = "alphabetic"; 
+     context.textAlign = "left"; 
+      
+     // get size data (height depends only on font size) 
+     var metrics = context.measureText( message ); 
+     var textWidth = metrics.width; 
+     
+     var cx = canvas.width / 2; 
+     var cy = canvas.height / 2; 
+     var tx = textWidth/ 2.0; 
+     var ty = fontsize / 2.0; 
+  
+     // then adjust for the justification 
+     if ( vAlign == "bottom") 
+         ty = 0; 
+     else if (vAlign == "top") 
+         ty = fontsize; 
+      
+     if (hAlign == "left") 
+         tx = textWidth; 
+     else if (hAlign == "right") 
+         tx = 0; 
+      
+     // the DESCENDER_ADJUST is extra height factor for text below baseline: g,j,p,q. since we don't know the true bbox 
+     roundRect(context, cx - tx , cy + ty + 0.28 * fontsize,  
+             textWidth, fontsize * DESCENDER_ADJUST, radius, borderThickness, borderColor, fillColor); 
+      
+     // text color.  Note that we have to do this AFTER the round-rect as it also uses the "fillstyle" of the canvas 
+     context.fillStyle = getCanvasColor(textColor); 
+  
+     context.fillText( message, cx - tx, cy + ty); 
+   
+    // canvas contents will be used for a texture 
+     var texture = new THREE.Texture(canvas) 
+     texture.needsUpdate = true; 
+     
+	 
+	
+/*	
+	var dynamicTexture = new THREEx.DynamicTexture(512,512)
+	dynamicTexture.context.font	= "60px Times";
+	dynamicTexture.texture.anisotropy = renderer.getMaxAnisotropy()
+	// update the text
+	dynamicTexture.clear()
+	dynamicTexture.drawText(message,256, 256, 'white');
+	dynamicTexture.needsUpdate = true;
+*/
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+     var spriteMaterial = new THREE.SpriteMaterial( { map: texture } ); 
+     this.Texto2D = new THREE.Sprite( spriteMaterial );	
+      
+     // we MUST set the scale to 2:1.  The canvas is already at a 2:1 scale, 
+     // but the sprite itself is square: 1.0 by 1.0 
+     // Note also that the size of the scale factors controls the actual size of the text-label 
+     this.Texto2D.scale.set(EL,EA,1); 
+      
+     // set the sprite's position.  Note that this position is in the CENTER of the sprite 
+     this.Texto2D.position.set(x, y, z); 
+     cena.add(this.Texto2D); 
+} 
+  
+function roundRect(ctx, x, y, w, h, r, borderThickness, borderColor, fillColor)  
+{ 
+     // no point in drawing it if it isn't going to be rendered 
+     if (fillColor == undefined && borderColor == undefined)  
+         return; 
+  
+     x -= borderThickness + r; 
+     y += borderThickness + r; 
+     w += borderThickness * 2 + r * 2; 
+     h += borderThickness * 2 + r * 2; 
+      
+     ctx.beginPath(); 
+     ctx.moveTo(x+r, y); 
+     ctx.lineTo(x+w-r, y); 
+     ctx.quadraticCurveTo(x+w, y, x+w, y-r); 
+     ctx.lineTo(x+w, y-h+r); 
+     ctx.quadraticCurveTo(x+w, y-h, x+w-r, y-h); 
+     ctx.lineTo(x+r, y-h); 
+     ctx.quadraticCurveTo(x, y-h, x, y-h+r); 
+     ctx.lineTo(x, y-r); 
+     ctx.quadraticCurveTo(x, y, x+r, y); 
+     ctx.closePath(); 
+      
+     ctx.lineWidth = borderThickness; 
+  
+     // background color 
+     // border color 
+  
+     // if the fill color is defined, then fill it 
+     if (fillColor != undefined) { 
+         ctx.fillStyle = getCanvasColor(fillColor); 
+         ctx.fill(); 
+     } 
+      
+     if (borderThickness > 0 && borderColor != undefined) { 
+         ctx.strokeStyle = getCanvasColor(borderColor); 
+         ctx.stroke(); 
+     } 
+} 
 
+function getCanvasColor ( color ) { 
+     return "rgba(" + color.r + "," + color.g + "," + color.b + "," + color.a + ")"; 
+} 
 
+function UpdateTexto2D(nome)
+{
+	cena.remove(nome);
+}
+
+// função de arredondamento
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
 
